bfce333a9fce52ba0d5871058160740145b9dd9f
Lawrence Ho <lawrenceho@noreply.codeberg.org>
2024-05-23 15:20:22 +0000

    Update nvim config

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,6 +1,7 @@
 return {
   {
     "NeogitOrg/neogit",
+    commit = vim.fn.has("nvim-0.10") == 0 and "bc0c609e3568a171e0549b449aa1b2b4b5b20e8c" or nil,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
