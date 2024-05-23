72d06c8c0b6e4a8d5eaea711dffc231c5ed951dc
Lawrence Ho <203431+lawrenceho@users.noreply.github.com>
2024-05-23 15:20:22 +0000

    Update nvim config

--- .config/nvim/lua/plugins/pin.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- Pin until upgrading to next version of Neovim
-  -- { "example/example.nvim", pin = vim.fn.has("nvim-0.10") == 0 },
-  { "akinsho/bufferline.nvim", pin = vim.fn.has("nvim-0.10") == 0 },
-  { "NeogitOrg/neogit", pin = vim.fn.has("nvim-0.10") == 0 },
-}
