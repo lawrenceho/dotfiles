e851265548158ac8715fd16d1d58f617bf533eb7
Lawrence Ho <lawrenceho@noreply.codeberg.org>
2024-05-23 15:20:21 +0000

    Update nvim config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set filetype for compose files
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  group = vim.api.nvim_create_augroup("compose_filetype", {}),
+  pattern = {
+    "compose*.yaml",
+    "compose*.yml",
+    "docker-compose*.yaml",
+    "docker-compose*.yml",
+  },
+  callback = function()
+    vim.opt_local.filetype = "yaml.docker-compose"
+  end,
+})
