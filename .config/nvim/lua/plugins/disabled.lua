73bb5f9f0aef35216baea2d5b1900e878f288436
Lawrence Ho <lawrenceho@noreply.codeberg.org>
2024-05-23 15:20:21 +0000

    Update nvim config

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "catppuccin", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "iamcco/markdown-preview.nvim", enabled = false },
   { "theHamsta/nvim-dap-virtual-text", enabled = false },
 }
