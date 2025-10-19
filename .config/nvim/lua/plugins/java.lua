83eeee9bd0950d0c0bca5bfe8b3419a6b78146a9
Lawrence Ho <lawrenceho@noreply.codeberg.org>
2025-10-19 14:50:09 +0000

    Update nvim config

--- .config/nvim/lua/plugins/java.lua
+++ .config/nvim/lua/plugins/java.lua
@@ -34,10 +34,6 @@ return {
             },
           },
         }
-        -- https://github.com/LazyVim/LazyVim/pull/5218
-        opts.capabilities = LazyVim.has("blink.cmp") and require("blink.cmp").get_lsp_capabilities()
-          or LazyVim.has("cmp-nvim-lsp") and require("cmp_nvim_lsp").default_capabilities()
-          or nil
         opts.on_attach = function()
           -- Inlay hints
           if LazyVim.opts("nvim-lspconfig").inlay_hints.enabled then
