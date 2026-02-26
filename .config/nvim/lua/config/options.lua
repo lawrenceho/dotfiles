febbbb587f297287a37b65aa2193f1b5653f9c65
Lawrence Ho <lawrenceho@noreply.codeberg.org>
2026-02-26 00:55:15 +0000

    Update nvim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,3 +7,22 @@ vim.g.autoformat = false
 
 -- Number of spaces tabs count for
 vim.opt.tabstop = 8
+
+-- Disable tmux refresh added in this PR: https://github.com/neovim/neovim/pull/36787
+-- Reading system clipboard would trigger security prompt in iTerm2
+if vim.env.TMUX ~= nil and vim.fn.executable("tmux") == 1 then
+  vim.g.clipboard = {
+    name = "tmux",
+    copy = {
+      ["+"] = "tmux load-buffer -w -",
+      ["*"] = "tmux load-buffer -w -",
+    },
+    paste = {
+      ["+"] = "tmux save-buffer -",
+      ["*"] = "tmux save-buffer -",
+    },
+  }
+
+  -- Sync with system clipboard
+  vim.opt.clipboard = "unnamedplus"
+end
